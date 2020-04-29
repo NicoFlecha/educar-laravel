26dac4c149fd60224bc32022d05bcd1f024c8911
Nico Flecha <nicoflechacontacto@gmail.com>
2020-04-28 22:54:13 -0300

    Empezando Controladores

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Exception;
+use App\Categoria;
+use App\Curso;
+use App\User;
+use App\Profesor;
 
 class CategoriaController extends Controller
 {
-    //
+    public function mostrarCategorias()
+    {
+        $categorias = Categoria::All();
+
+        dd ($categorias);
+    }
+
+    public function mostrarCursos ($idCurso)
+    {
+        try {
+            $categoria = Categoria::find($idCurso);
+            if ($categoria == null) {
+                throw new Exception("La Categoria " . $idCurso . " no existe");
+            }
+            $cursos = $categoria->cursos;
+        } catch (\Throwable $th) {
+            dd($th);
+        }
+        return $cursos;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/prueba', 'ProfesorController@prueba');
+Route::get('/categorias', 'CategoriaController@mostrarCategorias');
+Route::get('/categorias/{id}', 'CategoriaController@mostrarCursos');
